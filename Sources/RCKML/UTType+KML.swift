ea9325c7abda7d8c98291d6196c9abba0f785364
RCCoop <guthookhikes@gmail.com>
2023-02-08 11:54:25 -0500

    Added UTType.kml and .kmz

---
+++ Sources/RCKML/UTType+KML.swift
@@ -0,0 +1,21 @@
+//
+//  UTType+KML.swift
+//  
+//
+//  Created by Ryan Linn on 2/8/23.
+//
+
+import UniformTypeIdentifiers
+
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, *)
+public extension UTType {
+    static var kml: UTType {
+        let tags = UTType.types(tag: "kml", tagClass: .filenameExtension, conformingTo: .xml)
+        return tags.first(where: { $0.identifier.contains("google") }) ?? tags.first!
+    }
+    
+    static var kmz: UTType {
+        let tags = UTType.types(tag: "kmz", tagClass: .filenameExtension, conformingTo: nil)
+        return tags.first(where: { $0.identifier.contains("google") }) ?? tags.first!
+    }
+}
